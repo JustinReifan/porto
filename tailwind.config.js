1ab769fbd7aa32fd0c17855c87ed91bea439b317
Justin <justinereifanwijaya@gmail.com>
2023-12-14 20:44:56 +0700

    update dark mode + perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode : 'class',
   theme: {
     container:{
       center: true,
@@ -16,7 +17,8 @@ module.exports = {
         '2xl' : '1320px',
       }
     },
+    
   },
-  plugins: [],
+  plugins: ['prettier-plugin-tailwindcss'],
 }
 
